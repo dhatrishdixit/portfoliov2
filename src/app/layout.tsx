15e5d5287953822ae00ab980f8fb6483b2006550
Dhatish Singh Dixit <dhatrish29@gmail.com>
2026-09-06 18:40:02 +0530

    Testing dark mode is SSR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,17 +15,20 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script id="theme-script" strategy="beforeInteractive">
-        {
-          `
-            (function(){
-              const saved = localStorage.getItem(${storageKey});
-              const isDark = saved == "true" ? true : ( saved == "false" ? false : window.matchMedia("(prefers-color-scheme: dark)").matches);
-              document.documentElement.classList.toggle("dark",isDark);
-             })()
-          `
-        }
-      </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  const theme = localStorage.getItem('dark') || 'system';
+                  const isDark = theme === 'dark' || 
+                    (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+                  document.documentElement.classList.toggle('dark', isDark);
+                } catch (e) {}
+              })();
+            `,
+          }}
+        />
       </head>
          
       <body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import createGlobe from 'cobe';
 import { useEffect, useRef } from "react";
 import { useSpring } from "@react-spring/web";
 import { useTheme } from "@/hooks/useTheme";
+import { KineticText } from "@/components/ui/kinetic-text"
 
 
 const projects = [
