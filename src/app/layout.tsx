fe8b97e1010c49ddfacd6347a37070d4663305ba
Dhatish Singh Dixit <dhatrish29@gmail.com>
2026-09-12 23:25:12 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from '../components/theme-provider';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: "Dhatrish Singh Dixit — Software Engineer",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           >
             {children}
           </ThemeProvider>
+          <Analytics />
       </body>
     </html>
   );
