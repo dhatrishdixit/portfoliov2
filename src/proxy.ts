7926e2f4b8f445844dd38d25e5fe89aef90b52e6
Dhatish Singh Dixit <dhatrish29@gmail.com>
2026-09-15 02:48:57 +0530

    what a stupid issue with proxy so with src directory u have to place it inside root of src directory

---
+++ src/proxy.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import { redis } from './lib/redis';
+
+
+export async function proxy(request: NextRequest,event:NextFetchEvent) {
+  const userCountry = request.headers.get("x-vercel-ip-country");
+  const isAlreadyCounted = request.cookies.get("visited")?.value;
+  const response = NextResponse.next();
+
+  if(userCountry && !isAlreadyCounted){
+     event.waitUntil(redis.hincrby('visits_by_country',userCountry,1).catch(()=>{}));
+     response.cookies.set('visited', '1', { maxAge: 60 * 60 * 24 })
+  }
+  
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
