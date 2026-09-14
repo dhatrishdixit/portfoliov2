be6d3a5b8cca30bc8e7f10970523226f0381b3ca
Dhatish Singh Dixit <dhatrish29@gmail.com>
2026-09-14 19:57:07 +0530

    wtf is wrong with proxy

--- backupProxy.ts
+++ backupProxy.ts
@@ -2,21 +2,27 @@
 // If using Next.js 15 or earlier, name this file middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { redis } from './lib/redis';
+//import { redis } from './src/lib/redis';
 
 // 1. The main execution function
-export async function proxy(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const userCountry = request.headers.get("x-vercel-ip-country");
+  const isAlreadyCounted = request.cookies.get("visited")?.value;
+  
+  console.log(isAlreadyCounted);
+  console.log('testing')
 
   if(!userCountry) return NextResponse.next()
 
   console.log(userCountry);
 
-  const countryCount:number|null = await redis.get(userCountry);
-  const newCount = countryCount == null ? 1 : countryCount + 1;
+ // const countryCount:number|null = await redis.get(userCountry);
+ // const newCount = countryCount == null ? 1 : countryCount + 1;
 
-  await redis.set(userCountry,newCount);
+  //await redis.set(userCountry,newCount);
   // Continue with the original request lifecycle if conditions pass
+
+
   
   return NextResponse.next()
 }
@@ -24,5 +30,6 @@ export async function proxy(request: NextRequest) {
 // 2. The Matcher Config
 // Filters which paths this proxy function will execute on
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+ // matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ["/:path*"]
 }
