c4a84d410480ba08418cdd261830ddbca7a1640c
Dhatish Singh Dixit <dhatrish29@gmail.com>
2026-09-19 23:04:55 +0530

    changing proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,9 +6,11 @@ import { redis } from './lib/redis';
 export async function proxy(request: NextRequest,event:NextFetchEvent) {
   const userCountry = request.headers.get("x-vercel-ip-country");
   const isAlreadyCounted = request.cookies.get("visited")?.value;
+  console.log(isAlreadyCounted);
+  console.log(userCountry);
   const response = NextResponse.next();
 
-  if(userCountry && !isAlreadyCounted){
+  if(userCountry && isAlreadyCounted == undefined){
      event.waitUntil(redis.hincrby('visits_by_country',userCountry,1).catch(()=>{}));
      response.cookies.set('visited', '1', { maxAge: 60 * 60 * 24 })
   }
